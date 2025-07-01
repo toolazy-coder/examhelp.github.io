6521f0d7096568039d478bf3879dab6ddbfbe9f3
toolazy-coder <2023cse13musakalim@buie.ac.in>
2025-07-01 08:55:08 +0530

    3rd push
    firebase.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,20 +1,22 @@
-// firebase-init.js
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js';
-import { getStorage } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js';
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyD3lbh7gFC_mOsrC8qRCOc8PX71RQ1ngM4",
+  // Import the functions you need from the SDKs you need
+    import {initializeApp} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+    import {getAnalytics} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-analytics.js";
+    // TODO: Add SDKs for Firebase products that you want to use
+    // https://firebase.google.com/docs/web/setup#available-libraries
+
+    // Your web app's Firebase configuration
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    const firebaseConfig = {
+        apiKey: "AIzaSyD3lbh7gFC_mOsrC8qRCOc8PX71RQ1ngM4",
     authDomain: "examhelp-7939a.firebaseapp.com",
     projectId: "examhelp-7939a",
     storageBucket: "examhelp-7939a.firebasestorage.app",
     messagingSenderId: "799197837592",
     appId: "1:799197837592:web:17ec7a83bf5cc16665066d",
     measurementId: "G-H6JS3PYLTN"
-};
+  };
 
-
-const app = initializeApp(firebaseConfig);
-export const storage = getStorage(app);
-export const db = getFirestore(app);
-
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
